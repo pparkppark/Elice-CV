0d7bcba3ac5c9eab29b72b477ab98af6b49025c1
jsp <pparkppark@spc.co.kr>
2021-11-04 11:32:35 +0900

    opencv

--- helloworld.py
+++ helloworld.py
@@ -1,17 +1,4 @@
-def complicate_function(a):
-    a += 1000
-    a *= 0.4
-
-    '''
-        very complicated
-    '''
-
-    a = "String"
-    a *= 0.1
-    
-    return a
+import cv2
 
 if __name__ == "__main__":
-    a= 40
-    complicate_function(a)
-    print("second line")
+    print(cv2.__version__)
