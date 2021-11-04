2a65275621afb8fe8d7f6b194ae0e0dc1a92188e
jsp <pparkppark@spc.co.kr>
2021-11-04 10:44:44 +0900

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+def complicate_function(a):
+    a += 1000
+    a *= 0.4
+
+    '''
+        very complicated
+    '''
+
+    a = "String"
+    a *= 0.1
+    
+    return a
+
+if __name__ == "__main__":
+    a= 40
+    complicate_function(a)
